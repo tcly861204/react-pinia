33f3333f725f42429c78d2a1b6725961f63f2cf0
tcly861204 <356671808@qq.com>
2022-10-21 09:18:31 +0800

    🐛 fix: 完善前端

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,19 +1,18 @@
-declare module 'react-pinia' {
-  interface ProviderProps {
-    store: Record<string, Record<string, any>>
-    children: React.ReactNode
-  }
-  export interface createStoreOption {
-    state: () => Record<string, any>
-    actions?: Record<string, (this: Record<string, any>) => any>
-    getters?: Record<string, (state: Record<string, any>) => any>
-  }
-  export function Provider(props: ProviderProps): JSX.Element
-  export const createStore: (options: Record<string, createStoreOption>) => Record<string, any>
-  export const useStore: (
-    globalKey: string
-  ) => (storeKey?: string | Array<string>) => Record<string, any>
-  export const defineStore: (
-    options: createStoreOption
-  ) => (storeKey?: string | Array<string>) => Record<string, any>
+interface createStoreOption {
+  state: () => Record<string, any>;
+  actions?: Record<string, (this: Record<string, any>) => any>;
+  getters?: Record<string, (state: Record<string, any>) => any>;
 }
+declare const defineStore: (options: createStoreOption) => (storeKey?: string | Array<string>) => Record<string, any>;
+
+declare const version = "1.9.0";
+
+interface ProviderProps {
+  store: Record<string, Record<string, any>>;
+  children: React.ReactNode;
+}
+declare const Provider: ({ store, children, }: ProviderProps) => JSX.Element;
+declare const createStore: (options: Record<string, createStoreOption>) => Record<string, any>;
+declare const useStore: (globalKey: string, storeKey?: string | Array<string>) => Record<string, any>;
+
+export { Provider, ProviderProps, createStore, createStoreOption, defineStore, useStore, version };
