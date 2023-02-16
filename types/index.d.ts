700b4cf4576834bb5e6c6ee24f4c5aef69fbc738
tcly861204 <356671808@qq.com>
2023-02-16 21:48:12 +0800

    feat: ✨ 完善前端

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,33 @@
 interface createStoreOption {
-  state: () => Record<string, any>;
-  actions?: Record<string, (this: Record<string, any>) => any>;
-  getters?: Record<string, (state: Record<string, any>) => any>;
+  state: () => Record<string, any>
+  actions?: Record<string, (this: Record<string, any>, ...args: any) => any>
+  getters?: Record<string, (state: Record<string, any>) => any>
 }
-declare const defineStore: (options: createStoreOption) => (storeKey?: string | Array<string>) => Record<string, any>;
+declare const defineStore: (
+  options: createStoreOption
+) => (storeKey?: string | Array<string>) => Record<string, any>
 
-declare const version = "1.9.0";
+declare const version = '2.2.0'
 
 interface ProviderProps {
-  store: Record<string, Record<string, any>>;
-  children: React.ReactNode;
+  store: Record<string, Record<string, any>>
+  children: React.ReactNode
 }
-declare const Provider: ({ store, children, }: ProviderProps) => JSX.Element;
-declare const createStore: (options: Record<string, createStoreOption>) => Record<string, any>;
-declare const useStore: (globalKey: string, storeKey?: string | Array<string>) => Record<string, any>;
+declare const Provider: ({ store, children }: ProviderProps) => JSX.Element
+declare const defineModel: (options: createStoreOption) => createStoreOption
+declare const createStore: (options: Record<string, createStoreOption>) => Record<string, any>
+declare const useStore: (
+  globalKey: string,
+  storeKey?: string | Array<string>
+) => Record<string, any>
 
-export { Provider, ProviderProps, createStore, createStoreOption, defineStore, useStore, version };
+export {
+  Provider,
+  ProviderProps,
+  createStore,
+  createStoreOption,
+  defineModel,
+  defineStore,
+  useStore,
+  version,
+}
