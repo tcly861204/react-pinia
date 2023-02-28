ac3db97318b109e60cf8b5e6cbe3bc5dd5537049
tcly861204 <356671808@qq.com>
2023-02-28 19:11:29 +0800

    fix: 🐛 完善前端

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,27 +1,46 @@
-declare const storageType: ['localStorage', 'sessionStorage'];
-type Persist = {
-    key: string;
-    storage?: typeof storageType[number];
-};
+declare const storageType: ['localStorage', 'sessionStorage']
+declare type Persist = {
+  key: string
+  storage?: typeof storageType[number]
+}
 
 interface createStoreOption {
-    state: () => Record<string, any>;
-    actions?: Record<string, (this: Record<string, any>, ...args: any) => any>;
-    getters?: Record<string, (state: Record<string, any>) => any>;
-    persist?: Persist;
-    deep?: boolean;
+  // 定义状态
+  state: () => Record<string, any>
+  // 修改状态
+  actions?: Record<string, (this: Record<string, any>, ...args: any) => any>
+  // 监听状态更新生成新的状态
+  getters?: Record<string, (state: Record<string, any>) => any>
+  // 是否开启缓存持久化数据
+  persist?: Persist
+  // 是否深度监听数据
+  deep?: boolean
 }
-declare const defineStore: (options: createStoreOption) => (storeKey?: string | Array<string>) => Record<string, any>;
+declare const defineStore: (
+  options: createStoreOption
+) => (storeKey?: string | Array<string>) => Record<string, any>
 
-declare const version = "2.5.2";
+declare const version = '2.5.3'
 
 interface ProviderProps {
-    store: Record<string, Record<string, any>>;
-    children: React.ReactNode;
+  store: Record<string, Record<string, any>>
+  children: React.ReactNode
 }
-declare const Provider: ({ store, children }: ProviderProps) => JSX.Element;
-declare const defineModel: (options: createStoreOption) => createStoreOption;
-declare const createStore: (options: Record<string, createStoreOption>) => Record<string, any>;
-declare const useStore: (globalKey: string, storeKey?: string | Array<string>) => Record<string, any>;
+declare const Provider: ({ store, children }: ProviderProps) => JSX.Element
+declare const defineModel: (options: createStoreOption) => createStoreOption
+declare const createStore: (options: Record<string, createStoreOption>) => Record<string, any>
+declare const useStore: (
+  globalKey: string,
+  storeKey?: string | Array<string>
+) => Record<string, any>
 
-export { Provider, ProviderProps, createStore, createStoreOption, defineModel, defineStore, useStore, version };
+export {
+  Provider,
+  ProviderProps,
+  createStore,
+  createStoreOption,
+  defineModel,
+  defineStore,
+  useStore,
+  version,
+}
