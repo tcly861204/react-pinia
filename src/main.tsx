a4a4bbd5fba7dddae56d4d626ad02ef8243ef18d
tcly861204 <tcly861204@hotmail.com>
2024-11-22 11:53:30 +0800

    feat: ✨ 完善前端

--- src/main.tsx
+++ src/main.tsx
@@ -9,35 +9,3 @@ ReactDOM.render(
   </Provider>,
   root
 )
-
-// import ReactDOM from 'react-dom'
-// import { defineStore } from '../pinia/main'
-// const root = document.querySelector('#root')
-// const useStore = defineStore<{
-//   count: number
-//   actions: {
-//     add: () => void
-//   }
-// }>({
-//   state: () => {
-//     return {
-//       count: 10
-//     }
-//   },
-//   actions: {
-//     add() {
-//       this.count += 1
-//     }
-//   }
-// })
-
-// function App() {
-//   const store = useStore()
-//   return <section>
-//     <p>count: {store.count}</p>
-//     <div>
-//       <input value={store.count} onChange={store.add} />
-//     </div>
-//   </section>
-// }
-// ReactDOM.render(<App />, root)
